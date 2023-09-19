49bf87dc8329b59d4d6bec3b0b5de992a8d22e5b
GastonYasuda <gastonyasuda@gmail.com>
2023-09-19 20:39:57 -0300

    google sheet - json

--- src/Context/ApiContext.jsx
+++ src/Context/ApiContext.jsx
@@ -1,17 +1,43 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 
 export const ApiContext = createContext()
 
 const ApiProvider = ({ children }) => {
 
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+
+        fetch('https://sheets.googleapis.com/v4/spreadsheets/1-RTFa9iwdSs10uRBj0_APNBjztWpzzFT4G8XG3CPFTU/values/productos!A:F?key=AIzaSyBmF3CzttYqD4KHz1VoLwCa_4qeQDwoIoU')
+            .then(response => response.json())
+            .then(data => {
+                const header = data.values[0];
+                const rows = data.values.slice(1);
+                const result = [];
+
+                rows.forEach(row => {
+                    const obj = {};
+                    header.forEach((key, index) => {
+                        obj[key.toLowerCase()] = row[index];
+                    });
+                    result.push(obj);
+                });
+                setData(result)
+                console.log(result);
+            })
+            .catch(error => console.error('Error:', error));
+
+    }, [])
+
     const suma = (num1, num2) => {
         return (num1 + num2)
     }
 
 
+
     return (
-        <ApiContext.Provider value={{ suma }}>
+        <ApiContext.Provider value={{ suma, data }}>
             {children}
         </ApiContext.Provider>
     )
--- src/View/ItemListContainer/ItemListContainer.jsx
+++ src/View/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,18 @@
-import React, { Fragment, useContext } from 'react'
+import React, { Fragment, useContext, useEffect } from 'react'
 import { ApiContext } from '../../Context/ApiContext'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-import data from '../../data/data.json'
+// import data from '../../data/data.json'
 
 const ItemListContainer = () => {
 
 
-    const { suma } = useContext(ApiContext)
+    const { suma, data } = useContext(ApiContext)
+
+    useEffect(() => {
+        console.log(suma(1, 3))
+    }, [])
 
 
     return (
@@ -16,12 +20,12 @@ const ItemListContainer = () => {
             {data.map((eachProduct, i) => {
                 return (
                     <Card style={{ width: '18rem' }} key={i}>
-                        <Card.Img variant="top" src={eachProduct.Imagen} />
+                        <Card.Img variant="top" src={eachProduct.imagen} />
                         <Card.Body>
-                            <Card.Title>{eachProduct.Marca}</Card.Title>
-                            <Card.Text>{eachProduct.Producto}</Card.Text>
-                            <Card.Text>{eachProduct.Stock}</Card.Text>
-                            <Card.Text>{eachProduct.Precio}</Card.Text>
+                            <Card.Title>{eachProduct.marca}</Card.Title>
+                            <Card.Text>{eachProduct.producto}</Card.Text>
+                            <Card.Text>{eachProduct.stock}</Card.Text>
+                            <Card.Text>{eachProduct.precio}</Card.Text>
 
                             <Button variant="primary">Go somewhere</Button>
 
